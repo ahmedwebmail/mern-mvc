f591879e5cbf4bfbe7604d0ce9062d5b05708c1b
ahmedwebmail <aakhaled.msc@gmail.com>
2025-09-04 19:34:44 +0100

    intro how to use of req in express

--- index.js
+++ index.js
@@ -7,107 +7,33 @@
 const express = require('express')
 const app = express()
 
-/**
- * Defining API endpoints for different HTTP methods using Express.
- * - GET    `/one`    : Returns a simple text response for GET requests.
- */
+app.get('/', (req, res)=>{
+    res.cookie("cookie_01", "01")
+    res.cookie("cookie_02", "02")
+    res.cookie("cookie_03", "03")
+    res.cookie("cookie_04", "04")
+    res.cookie("cookie_05", "05")
 
-app.get("/one", (req, res)=>{
+    res.header("header_01", "01")
 
-    let Person= [
-      {
-        "name": "Alice Johnson",
-        "city": "New York",
-        "occupation": "Software Engineer",
-        "email": "alice.johnson@example.com"
-      },
-      {
-        "name": "Brian Smith",
-        "city": "Los Angeles",
-        "occupation": "Graphic Designer",
-        "email": "brian.smith@example.com"
-      },
-      {
-        "name": "Catherine Lee",
-        "city": "Chicago",
-        "occupation": "Data Analyst",
-        "email": "catherine.lee@example.com"
-      },
-      {
-        "name": "David Martinez",
-        "city": "Houston",
-        "occupation": "Marketing Manager",
-        "email": "david.martinez@example.com"
-      },
-      {
-        "name": "Emily Davis",
-        "city": "Seattle",
-        "occupation": "UX Designer",
-        "email": "emily.davis@example.com"
-      }
+    res.end("this is simple string")
+})
+
+app.get('/custom-req', (req, res)=>{
+    let Person = [
+        {name: "Ahmed", city: "Dhaka", occupation: "Engineer"}
     ]
-    res.append("name", "Ahmed")
-    res.append("city", "Dhaka")
 
-    res.cookie("C1", "Cookie 1")
-    res.cookie("C2", "Cookie 2")
-    res.cookie("C3", "Cookie 3")
-    res.cookie("C4", "Cookie 4")
-
-    res.status(200).json(Person)
-});
-
-app.get("/download", (req, res)=>{
-    res.download("file.txt")
+    res.append("name", "Khaled Ahmed")
+    res.json(Person)
 })
 
-app.get("/redirect", (req, res)=>{
-    res.redirect("/one")
+app.get('/clear-cookie', (req, res)=>{
+    res.clearCookie("cookie_01")
+    res.end("Cleared")
 })
-
-/**
- * Defining API endpoints for different HTTP methods using Express.
- * - POST   `/two`    : Returns a simple text response for POST requests.
- */
-
-app.post("/two", (req, res)=>{
-    res.end("This is page two from post")
-});
-
-/**
- * Defining API endpoints for different HTTP methods using Express.
- * - POST   `/two`    : Returns a simple text response for POST requests.
- */
-app.put("/three", (req, res)=>{
-    res.end("This is page three from put")
-});
-
-/**
- * Defining API endpoints for different HTTP methods using Express.
- * - PUT    `/three`  : Returns a simple text response for PUT requests.
- */
-app.patch("/four", (req, res)=>{
-    res.end("This is page four from patch")
-});
-
-/**
- * Defining API endpoints for different HTTP methods using Express.
- *
- * - DELETE `/five`   : Returns a simple text response for DELETE requests.
- */
-app.delete("/five", (req, res)=>{
-    res.end("This is page five from delete")
-});
-
-app.get("/five", (req, res)=>{
-
-});
-
-/**
- * Starts the Express server and listens for incoming requests on the specified port.
- * - `app.listen(3000, ...)` binds and listens for connections on port 3000.
- */
 
 app.listen(3000, ()=>{
     console.log("Server is running on port")
 })
+
